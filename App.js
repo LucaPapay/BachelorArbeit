f54c97e17d7d5f7052d591ee9668096989e2bb96
Luca Papay <e11809626@student.tuwien.ac.at>
2022-03-23 13:33:17 +0100

    moves Screens to own Components, gives tabs icons

--- App.js
+++ App.js
@@ -2,40 +2,38 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {HomeScreen} from './Screens/HomeScreen';
+import {SettingsScreen} from './Screens/SettingsScreen';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => getIcons(route, focused, color, size),
+          tabBarActiveTintColor: 'tomato',
+          tabBarInactiveTintColor: 'gray',
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
         <Tab.Screen name="Settings" component={SettingsScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+function getIcons(route, focused, color, size) {
+  let iconName;
+
+  if (route.name === 'Home') {
+    iconName = focused
+      ? 'ios-information-circle'
+      : 'ios-information-circle-outline';
+  } else if (route.name === 'Settings') {
+    iconName = focused ? 'settings' : 'settings-outline';
+  }
+
+  return <Ionicons name={iconName} size={size} color={color} />;
+}
