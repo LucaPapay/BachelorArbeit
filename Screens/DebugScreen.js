475f6fc032959cb47d04758cae3eec626140ad36
Luca Papay <e11809626@student.tuwien.ac.at>
2022-04-07 13:43:43 +0200

    adds Ean API to debug Screen

--- Screens/DebugScreen.js
+++ Screens/DebugScreen.js
@@ -1,12 +1,15 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TextInput } from "react-native";
 import { useDispatch, useSelector } from "react-redux";
 import { initalState, nextId } from "../redux/actions";
 import Button from "../Components/Button";
+import React, { useState } from "react";
 
 export function DebugScreen() {
   const dispatch = useDispatch();
   let counter = useSelector((state) => state.idCounter);
   let store = useSelector((state) => state);
+  const [title, onChangeName] = React.useState("");
+  const [ean, setEan] = useState("885909128525");
 
   return (
     <View
@@ -23,6 +26,34 @@ export function DebugScreen() {
         Current ID: {counter} <Button color="tomato" title="+" onPress={() => dispatch(nextId())} />
       </Text>
       <Button color="tomato" title="Store" onPress={() => console.log(store)} />
+      <Button color="tomato" title="TEST api" onPress={() => testAPI()} />
+      <TextInput style={styles.input} onChangeText={setEan} value={ean} placeholder="EAN / UPC / ISBN" />
+      <Text>Name: {title}</Text>
     </View>
   );
+
+  function testAPI() {
+    fetch("https://api.upcitemdb.com/prod/trial/lookup?upc=" + ean)
+      .then((response) => response.json())
+      .then((json) => {
+        if (json.code === "OK") {
+          onChangeName(json.items[0].title);
+        } else {
+          onChangeName(json.message);
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
+
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    width: 300,
+    paddingHorizontal: 5,
+    backgroundColor: "white",
+    marginBottom: 5,
+  },
+});
