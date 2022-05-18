051379b017b0095f9f9a36af6d7b918714561a3f
Luca Papay <e11809626@student.tuwien.ac.at>
2022-05-18 10:53:21 +0200

    adds amount to EntryListEntry

--- Components/EntryListEntry.js
+++ Components/EntryListEntry.js
@@ -1,12 +1,13 @@
 import { useNavigation } from "@react-navigation/native";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { StyleSheet } from "react-native";
-import { Box, Text, HStack } from "native-base";
+import { Box, Text, HStack, VStack } from "native-base";
 
 export default function EntryListEntry({ entry, parentIds }) {
   const navigation = useNavigation();
 
   let amount = "";
+  let threshold = "";
   const hasAmountParameter = checkForAmount();
 
   return (
@@ -19,20 +20,37 @@ export default function EntryListEntry({ entry, parentIds }) {
       </HStack>
       <Box style={{ flex: 5, flexDirection: "row" }}>
         {hasAmountParameter ? (
-          <HStack mt={1.5} mr="2">
-            <Ionicons
-              name="add-circle-outline"
-              size={25}
-              color="#14213d"
-              onPress={() => navigation.push("Edit Amount", { name: entry.name, parentIds: parentIds, amount, entry })}
-            />
-            <Ionicons
-              name="remove-circle-outline"
-              size={25}
-              color="#14213d"
-              onPress={() => navigation.push("Edit Amount", { name: entry.name, parentIds: parentIds, amount, entry })}
-            />
-          </HStack>
+          <VStack mt={0} mr="2">
+            <HStack>
+              <Ionicons
+                name="add-circle-outline"
+                size={25}
+                color="#14213d"
+                onPress={() =>
+                  navigation.push("Edit Amount", { name: entry.name, parentIds: parentIds, amount, entry })
+                }
+              />
+              <Ionicons
+                name="remove-circle-outline"
+                size={25}
+                color="#14213d"
+                onPress={() =>
+                  navigation.push("Edit Amount", { name: entry.name, parentIds: parentIds, amount, entry })
+                }
+              />
+            </HStack>
+            <Box ml="2">
+              {amount >= threshold ? (
+                <Text color="green.500">
+                  {amount}/{threshold}
+                </Text>
+              ) : (
+                <Text color="red.500">
+                  {amount}/{threshold}
+                </Text>
+              )}
+            </Box>
+          </VStack>
         ) : (
           <HStack mt={1.5} mr="2">
             <Ionicons name="add-circle-outline" size={25} color="transparent" />
@@ -62,6 +80,9 @@ export default function EntryListEntry({ entry, parentIds }) {
       if (parameter.name === "Amount") {
         amount = parameter.value;
         found = true;
+      }
+      if (parameter.name === "Threshold") {
+        threshold = parameter.value;
       }
     });
     return found;
